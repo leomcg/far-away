ca13b0de250f0b0d95d3f1920c4320fdbf9f78b3
leomcg <leomcg@gmail.com>
2025-05-04 21:33:13 -0300

    creting form

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { useState } from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+const quantities = Array.from({ length: 20 }, (_, i) => i + 1);
+
 export default function App() {
   return (
     <div className="app">
@@ -14,15 +23,72 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  // const [itens, setItens] = useState(initialItems);
+  // setItens((itens) => [
+  //   ...itens,
+  //   {
+  //     id: Date.now(),
+  //     description,
+  //     quantity,
+  //     packed: false,
+  //   },
+  // ]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!description) return;
+
+    setDescription("");
+    setQuantity(1);
+  };
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {quantities.map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
